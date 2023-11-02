b3f72e406b32f749491676f46be3e7ba6caecc0e
xucian <5688727+xucian@users.noreply.github.com>
2023-11-02 19:24:37 +0200

    potential fixes with autobuild, now building on server

--- release.sh
+++ release.sh
@@ -23,23 +23,24 @@ echo 'Bump version & push, including tags'
 bump2version "$part"
 git push --follow-tags
 
-echo "You might be asked multiple times for the ssh key, even in a GUI OpenSSH window. Proceed each time"
-echo "You might be asked multiple times for the ssh key, even in a GUI OpenSSH window. Proceed each time"
+# Update: only pushing tags now
+# echo "You might be asked multiple times for the ssh key, even in a GUI OpenSSH window. Proceed each time"
+# echo "You might be asked multiple times for the ssh key, even in a GUI OpenSSH window. Proceed each time"
 
-# This is only needed once, but it's good to keep in case this script will only execute once
-git config 'lfs.https://github.com/pizzaserved/mjapi.io/info/lfs.locksverify' false
-git config 'lfs.git@github.com:pizzaserved/mjapi.io/info/lfs.locksverify' false
+# # This is only needed once, but it's good to keep in case this script will only execute once
+# git config 'lfs.https://github.com/pizzaserved/mjapi.io/info/lfs.locksverify' false
+# git config 'lfs.git@github.com:pizzaserved/mjapi.io/info/lfs.locksverify' false
 
-# Proably this is the only one needed
-# https://github.com/git-lfs/git-lfs/issues/2269#issuecomment-304299990
-git config lfs.https://github.com.locksverify false
+# # Proably this is the only one needed
+# # https://github.com/git-lfs/git-lfs/issues/2269#issuecomment-304299990
+# git config lfs.https://github.com.locksverify false
 
-# NOTE: THIS ONLY WORKS ON BASH! FOR SOME REASON, RUNNING THIS COMMAND MANUALLY IN PS (and probably cmd) FAILS WITH: error: failed to push some refs to 'github.com:xucian/com.thefallengames.needle.git
-echo "Push to the $new_branch branch via git subtree magic"
-# # Pushing directly to a branch, no intermediary steps! 
-# # Thanks https://github.com/IvanMurzak/Unity-Package-Template/blob/master/gitSubTreePushToUPM.bat
-# git subtree push --prefix dist/mjapi origin "$new_branch"
+# # NOTE: THIS ONLY WORKS ON BASH! FOR SOME REASON, RUNNING THIS COMMAND MANUALLY IN PS (and probably cmd) FAILS WITH: error: failed to push some refs to 'github.com:xucian/com.thefallengames.needle.git
+# echo "Push to the $new_branch branch via git subtree magic"
+# # # Pushing directly to a branch, no intermediary steps! 
+# # # Thanks https://github.com/IvanMurzak/Unity-Package-Template/blob/master/gitSubTreePushToUPM.bat
+# # git subtree push --prefix dist/mjapi origin "$new_branch"
 
-# Update: this actually creates the branch. Note that the build branch is not tagged as its name already implies its version. The tag is for the sources
-git subtree split --prefix=dist/mjapi --squash -b "$new_branch"
-git push origin "$new_branch"
+# # Update: this actually creates the branch. Note that the build branch is not tagged as its name already implies its version. The tag is for the sources
+# git subtree split --prefix=dist/mjapi --squash -b "$new_branch"
+# git push origin "$new_branch"

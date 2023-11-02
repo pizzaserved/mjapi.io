5a76b09853f4a0555b96ceedef57ef8c11e52b94
xucian <5688727+xucian@users.noreply.github.com>
2023-11-02 18:59:03 +0200

    potential fix where branches are now created

--- release.sh
+++ release.sh
@@ -17,7 +17,8 @@ echo 'If you get an error, install (and restart the terminal): pip install bump2
 # src: https://pypi.org/project/bump2version/
 new_vers=$(bump2version --dry-run --list "$part" | grep new_version | sed -r s,"^.*=",,)
 new_vers_str="v$new_vers"
-new_branch="$new_vers_str"
+new_branch="build-$new_vers_str"
+# The sources are marked with a tag
 echo 'Bump version & push, including tags'
 bump2version "$part"
 git push --follow-tags
@@ -34,7 +35,11 @@ git config 'lfs.git@github.com:pizzaserved/mjapi.io/info/lfs.locksverify' false
 git config lfs.https://github.com.locksverify false
 
 # NOTE: THIS ONLY WORKS ON BASH! FOR SOME REASON, RUNNING THIS COMMAND MANUALLY IN PS (and probably cmd) FAILS WITH: error: failed to push some refs to 'github.com:xucian/com.thefallengames.needle.git
-echo 'Push to the new version branch via gut subtree magic'
-# Pushing directly to a branch, no intermediary steps! 
-# Thanks https://github.com/IvanMurzak/Unity-Package-Template/blob/master/gitSubTreePushToUPM.bat
-git subtree push --prefix mjapi/dist/mjapi origin "$new_branch"
+echo "Push to the $new_branch branch via git subtree magic"
+# # Pushing directly to a branch, no intermediary steps! 
+# # Thanks https://github.com/IvanMurzak/Unity-Package-Template/blob/master/gitSubTreePushToUPM.bat
+# git subtree push --prefix mjapi/dist/mjapi origin "$new_branch"
+
+# Update: this actually creates the branch. Note that the build branch is not tagged as its name already implies its version. The tag is for the sources
+git subtree split --prefix=mjapi/dist/mjapi --squash -b "$new_branch"
+git push origin "$new_branch"

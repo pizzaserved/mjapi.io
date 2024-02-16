24ad1d8fc8ebd9150acd49b6794abbdd69cfea80
mjapi.io <hi@mjapi.io>
2024-02-16 17:02:54 +0000

    Updates

---
+++ blog/assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1708102923',resources: [
+      '/blog/assets/css/jekyll-theme-chirpy.css',
+      '/blog/',
+      
+        '/blog/categories/',
+      
+        '/blog/tags/',
+      
+        '/blog/archives/',
+      
+        '/blog/about/',
+      
+
+      
+      
+    ],allowHosts: [
+      
+          'fonts.googleapis.com',
+          'fonts.gstatic.com',
+          'fonts.googleapis.com',
+          'cdn.jsdelivr.net',
+          'polyfill.io'
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+

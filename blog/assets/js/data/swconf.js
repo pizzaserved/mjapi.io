651f3061408f495b12915e75be27f55429e846a3
mjapi.io <hi@mjapi.io>
2025-07-19 11:01:14 +0000

    Updates

--- blog/assets/js/data/swconf.js
+++ blog/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1752922511',resources: [
+    cacheName: 'chirpy-1752922835',resources: [
       '/blog/assets/css/jekyll-theme-chirpy.css',
       '/blog/',
       
